873e5a701410aed3668c2942e85b5a04023da3a9
uor16x <u0r.home@gmail.com>
2022-06-22 00:02:45 +0300

    update systray;

--- index.py
+++ index.py
@@ -11,4 +11,10 @@ def set_icon_text(icon_image_name: str, text: int):
   drawed = ImageDraw.Draw(new_img)
   drawed.text((5, 5), f"{text}", fill=(255,255,255), font = font_type)
   new_img.save(icon_image_name)
-  
+
+def update_systray(icon_image_name: str):
+  if not systray:
+    systray = SysTrayIcon("volume_tray.ico", "Example tray icon", ())
+    systray.start()
+  else:
+    systray.update(icon=icon_image_name)
