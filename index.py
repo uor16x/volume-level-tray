0bf4efe4a819d9987d285227b057eb821272a651
uor16x <u0r.home@gmail.com>
2022-06-22 00:15:31 +0300

    main loop;

--- index.py
+++ index.py
@@ -2,9 +2,7 @@ from infi.systray import SysTrayIcon
 from PIL import Image, ImageDraw,ImageFont
 import time
 
-image_name = "volume_tray.ico"
 font_type = ImageFont.truetype("arial.ttf", 55)
-systray = None
 
 def set_icon_text(icon_image_name: str, text: int):
   new_img = Image.new('RGBA', (65, 65), color = (255, 255, 255, 0))
@@ -13,8 +11,19 @@ def set_icon_text(icon_image_name: str, text: int):
   new_img.save(icon_image_name)
 
 def update_systray(icon_image_name: str):
-  if not systray:
-    systray = SysTrayIcon("volume_tray.ico", "Example tray icon", ())
+  if systray is None:
+    systray = SysTrayIcon(icon_image_name, "Example tray icon", ())
     systray.start()
   else:
     systray.update(icon=icon_image_name)
+
+def main():
+  default_icon_name = "volume_tray.ico"
+  set_icon_text(default_icon_name, 25)
+  systray = SysTrayIcon(default_icon_name, "", ())
+  systray.start()
+  while True:
+    systray.update(icon=default_icon_name)
+    time.sleep(5)
+
+main()
