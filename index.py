2dda48eef558b5ba072f9b6a61e2086a8b503c38
uor16x <u0r.home@gmail.com>
2022-06-21 23:49:44 +0300

    set icon text;

--- index.py
+++ index.py
@@ -1,6 +1,14 @@
-def main():
-    print('hello')
+from infi.systray import SysTrayIcon
+from PIL import Image, ImageDraw,ImageFont
+import time
 
+image_name = "volume_tray.ico"
+font_type = ImageFont.truetype("arial.ttf", 55)
+systray = None
 
-if __name__ == "__main__":
-    main()
+def set_icon_text(icon_image_name: str, text: int):
+  new_img = Image.new('RGBA', (65, 65), color = (255, 255, 255, 0))
+  drawed = ImageDraw.Draw(new_img)
+  drawed.text((5, 5), f"{text}", fill=(255,255,255), font = font_type)
+  new_img.save(icon_image_name)
+  
