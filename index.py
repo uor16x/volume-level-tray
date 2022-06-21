9e152bb42d6ca25fb5d999504cf972650aaeff6f
uor16x <u0r.home@gmail.com>
2022-06-22 02:28:48 +0300

    custom_shutdown

--- index.py
+++ index.py
@@ -1,7 +1,8 @@
+import sys
 from infi.systray import SysTrayIcon
+from infi.systray.traybar import PostMessage, WM_CLOSE
 from PIL import Image, ImageDraw,ImageFont
 import subprocess
-from subprocess import Popen, PIPE
 import time
 
 # Constants
@@ -28,16 +29,27 @@ def get_volume_level():
   out = subprocess.check_output('powershell ./get_volume.ps1', shell=True, encoding='utf8').strip()
   return int(float(out.replace(',', '.')) * 100)
 
+def custom_shutdown(self):
+  if not self._hwnd:
+      return
+  PostMessage(self._hwnd, WM_CLOSE, 0, 0)
+
 
 def main():
   default_icon_name = "volume_tray.ico"
   set_icon_text(default_icon_name, 25)
   menu_options = (("Toggle mute", None, mute),)
-  systray = SysTrayIcon(default_icon_name, "", menu_options)
+  systray = None
+  SysTrayIcon.shutdown = custom_shutdown
+  def on_quit(self):
+    systray.shutdown()
+    raise SystemExit(0)
+
+  systray = SysTrayIcon(default_icon_name, "", menu_options, on_quit=on_quit)
   systray.start()
   print(get_volume_level())
   while True:
-    systray.update(icon=default_icon_name)
+    # systray.update(icon=default_icon_name)
     time.sleep(0.5)
 
 main()
