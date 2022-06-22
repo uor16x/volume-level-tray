2932e36d4d99bb91d20134f80b9da5f06aa7fbb7
uor16x <u0r.home@gmail.com>
2022-06-23 01:33:13 +0300

    TaskBarIcon impl;

--- index.py
+++ index.py
@@ -1,14 +1,12 @@
-from infi.systray import SysTrayIcon
-from infi.systray.traybar import PostMessage, WM_CLOSE
+import sys
 from PIL import Image, ImageDraw,ImageFont
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume, ISimpleAudioVolume
-import subprocess
+import wx.adv
+import wx
 import time
 
-# Constants
-font_type = ImageFont.truetype("arial.ttf", 55)
 
 class Audio:
   def __init__(self):
@@ -19,47 +17,53 @@ class Audio:
   def getVolume(self):
     return int(round(self.volume.GetMasterVolumeLevelScalar() * 100))
 
-def set_icon_text(icon_image_name: str, text: int):
-  new_img = Image.new('RGBA', (65, 65), color = (255, 255, 255, 0))
-  drawed = ImageDraw.Draw(new_img)
-  drawed.text((5, 5), f"{text}", fill=(255,255,255), font = font_type)
-  new_img.save(icon_image_name)
 
-def update_systray(icon_image_name: str):
-  if systray is None:
-    systray = SysTrayIcon(icon_image_name, "Example tray icon", ())
-    systray.start()
-  else:
-    systray.update(icon=icon_image_name)
+class TaskBarIcon(wx.adv.TaskBarIcon):
+    tray_icon = 'volume_tray.ico'
+    font_type = ImageFont.truetype("arial.ttf", 55)
 
+    def __init__(self, frame):
+        self.frame = frame
+        super(TaskBarIcon, self).__init__()
+        self.update()
 
-def get_volume_level():
-  out = subprocess.check_output('powershell ./get_volume.ps1', shell=True, encoding='utf8').strip()
-  return int(float(out.replace(',', '.')) * 100)
+    @staticmethod
+    def refresh_icon(volume):
+      new_img = Image.new('RGBA', (65, 65), color = (255, 255, 255, 0))
+      drawed = ImageDraw.Draw(new_img)
+      drawed.text((5, 5), f"{volume}", fill=(255,255,255), font = TaskBarIcon.font_type)
+      new_img.save(TaskBarIcon.tray_icon)
 
-def custom_shutdown(self):
-  if not self._hwnd:
-      return
-  PostMessage(self._hwnd, WM_CLOSE, 0, 0)
+    def CreatePopupMenu(self):
+        menu = wx.Menu()
+        exitItem = wx.MenuItem(menu, -1, 'Exit')
+        menu.Bind(wx.EVT_MENU, self.on_exit, id=exitItem.GetId())
+        menu.Append(exitItem)
+        return menu
 
+    def update(self):
+      icon = wx.Icon(TaskBarIcon.tray_icon)
+      self.SetIcon(icon, '')
+
+    def on_exit(self, event):
+        wx.CallAfter(self.Destroy)
+        self.frame.Close()
+        sys.exit(0)
+
+class App(wx.App):
+    def OnInit(self):
+        frame=wx.Frame(None)
+        self.SetTopWindow(frame)
+        self.icon = TaskBarIcon(frame)
+        audio = Audio()
+        while True:
+          currVolume = audio.getVolume()
+          TaskBarIcon.refresh_icon(currVolume)
+          self.icon.update()
+          time.sleep(.5)
 
 def main():
-  audio = Audio()
-  default_icon_name = "volume_tray.ico"
-  set_icon_text(default_icon_name, 25)
-  menu_options = (("Toggle mute", None, None),)
-  systray = None
-  SysTrayIcon.shutdown = custom_shutdown
-  def on_quit(self):
-    systray.shutdown()
-    raise SystemExit(0)
-
-  systray = SysTrayIcon(default_icon_name, "", menu_options, on_quit=on_quit)
-  systray.start()
-  print(get_volume_level())
-  volume = init_audio()
-  while True:
-    # systray.update(icon=default_icon_name)
-    time.sleep(0.5)
+  app = App(False)
+  app.MainLoop()
 
 main()
