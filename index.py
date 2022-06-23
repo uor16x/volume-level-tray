3c59b5e96d3b4eab9e13dfb5e4b90c8fbe80a80a
uor16x <u0r.home@gmail.com>
2022-06-23 15:25:34 +0300

    default icon

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from threading import Thread
+from os.path import exists
 import wx.adv
 import wx
 import time
@@ -30,6 +31,8 @@ class TaskBarIcon(wx.adv.TaskBarIcon):
     def __init__(self, frame):
       wx.adv.TaskBarIcon.__init__(self)
       self.frame = frame
+      if not exists(TaskBarIcon.tray_icon):
+        TaskBarIcon.refresh_icon(0, False)
       self.update_icon()
 
     def CreatePopupMenu(self):
